2101d3f51cbb659b61f2c045b11758a60e5c0481
Rishikesh623 <rishikeshkumar39936@gmail.com>
2025-04-13 19:39:06 +0530

    fixed errors

--- server/routes/watchListRoutes.js
+++ server/routes/watchListRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { getAllWatchLists, createWatchList, renameWatchList, deleteAllWatchLists, deleteWatchList,
     getWatchListItems, addItemToWatchList, removeItemFromWatchList } = require('../controllers/watchListController');
-const { authUser, checkSharedWatchlistPermissions } = require("../middleware/authMiddleware");
+const { authUser } = require("../middleware/authMiddleware");
+const { checkSharedWatchlistPermissions } = require("../middleware/checkSharedWatchlistPermissions");
 
 const {createSharedWatchList,renameSharedWatchList,addMember,addItem,removeItem} = require("../controllers/sharedWatchListController");
 const router = express.Router();
@@ -16,8 +17,8 @@ router.patch('/addToWatchlist/:watchlist_id', authUser, addItemToWatchList);
 router.patch('/:watchlist_id/:item_id', authUser, removeItemFromWatchList);
 
 //to share 
-rouet.post("/shared",authUser,createSharedWatchList);
-rouet.patch("/shared/rename/:watchlist_id/:new_title",authUser,renameSharedWatchList);
+router.post("/shared",authUser,createSharedWatchList);
+router.patch("/shared/rename/:watchlist_id/:new_title",authUser,renameSharedWatchList);
 router.patch("/shared/:watchlist_id",authUser,checkSharedWatchlistPermissions('isOwner'),addMember);
 router.put('/shared/add/:watchlist_id', checkSharedWatchlistPermissions('canAdd'),addItem);
 router.delete('/shared/remove/:watchlist_id/:item_id', checkSharedWatchlistPermissions('canRemove'),removeItem);
