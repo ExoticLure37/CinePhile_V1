d4077821deab414799a7d63bc7f0d15aa362d384
ExoticLure37 <aryanamih041@gmail.com>
2026-08-24 01:28:58 +0530

    Fix email errors

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,28 +1,27 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 module.exports = async (email, subject, text) => {
-    try {
-        const transporter = nodemailer.createTransport({
-            host: process.env.HOST,
-            service: process.env.SERVICE,
-            port: Number(process.env.EMAIL_PORT),
-            secure: Boolean(process.env.SECURE),
-            auth: {
-                user: process.env.USER,
-                pass: process.env.PASS
-            }
-        })
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST,
+      service: process.env.SERVICE,
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
 
-        await transporter.sendMail({
-            from: process.env.USER,
-            to: email,
-            subject: subject,
-            text: text
-        })
+    await transporter.sendMail({
+      from: process.env.USER,
+      to: email,
+      subject: subject,
+      text: text,
+    });
 
-        console.log("Email sent successfully")
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
+    console.log("Email sent successfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
